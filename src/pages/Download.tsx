f07e5f6b9f770e083f71007f3d92c45f14d5163f
mundane <799699348@qq.com>
2025-01-17 14:29:12 +0800

    更新至1.6.0

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -4,22 +4,22 @@ import { DownloadOutlined } from "@ant-design/icons";
 
 const { Link } = Typography;
 
-const Download: FC = () => {
+const Download = () => {
   return (
     <div className="h-full bg-gradient-to-br from-[#ee9ca7] to-[#ffdde1]">
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-col items-center space-y-6 md:flex-row md:justify-around md:space-y-0 md:space-x-4">
           <DownloadCard
             title="Windows"
-            href="https://cdn.dreamfree.xyz/updater/RedArchive-Windows-1.5.3-Setup.exe"
+            href="https://cdn.dreamfree.xyz/updater/RedArchive-Windows-1.6.0-Setup.exe"
           />
           <DownloadCard
             title="Mac Intel芯片"
-            href="https://cdn.dreamfree.xyz/updater/RedArchive-Mac-x64-1.5.3-Installer.dmg"
+            href="https://cdn.dreamfree.xyz/updater/RedArchive-Mac-x64-1.6.0-Installer.dmg"
           />
           <DownloadCard
             title="Mac M芯片"
-            href="https://cdn.dreamfree.xyz/updater/RedArchive-Mac-arm64-1.5.3-Installer.dmg"
+            href="https://cdn.dreamfree.xyz/updater/RedArchive-Mac-arm64-1.6.0-Installer.dmg"
           />
         </div>
       </div>
